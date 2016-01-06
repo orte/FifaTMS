c65732fa5a19f6ad681bb6df4c57e70481c9584d
orte <orte.jon@opendeusto.es>
2016-01-06 19:39:43 +0100

    todo terminado

--- src/comun/NoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package comun;
-
-public class NoEncontradoException extends Exception{
-	
-	public String getMessage(){
-		return "No se ha encontrado el elemento buscado, inténtelo de nuevo";
-	}
-
-}
