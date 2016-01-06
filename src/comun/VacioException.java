ec217c8dd010110c8ade09ab1dca28afc3c55251
orte <orte.jon@opendeusto.es>
2016-01-06 19:35:36 +0100

    sacabó

--- src/comun/VacioException.java
+++ src/comun/VacioException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package comun;
 
 /**
@@ -14,17 +13,4 @@ public class VacioException extends Exception{
 	private static final long serialVersionUID = 1L;
 
 }
-=======
-package comun;
 
-public class VacioException extends Exception{
-
-	String clase;
-	public VacioException(String cls){
-		clase=cls;
-	}
-	public String getMessage(){
-		return "No hay ningún "+clase+" dado de alta aún";
-	}
-}
->>>>>>> 7ec2ec74481d9fd4f244615e66beee7ba8277ac2
